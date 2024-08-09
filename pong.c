2f3d7f06019b1639cf73c012c6f0c471288b54e4
Raul Fonseca <114365146+RaulFonseca002@users.noreply.github.com>
2024-08-08 21:44:44 -0300

    Update pong.c
    remove unnecessary code

--- pong.c
+++ pong.c
@@ -69,7 +69,7 @@ Ball initBall(){
 
 int check_collision(SDL_Rect* a, SDL_Rect* b) {
 
-	return 1 * ((a->x < (b->x + b->w))) * (((a->x + a->w) > b->x)) * (!(a->y > (b->y + b->h))) * (!((a->y + a->h) < b->y));
+	return ((a->x < (b->x + b->w))) * (((a->x + a->w) > b->x)) * (!(a->y > (b->y + b->h))) * (!((a->y + a->h) < b->y));
 }
 
 void invertBallX(Ball *ball){
